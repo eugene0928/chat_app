238c3ab78eb498e95a525314d5e65961daf7b7a7
eugene0928 <umid09tuxtayev@gmail.com>
2022-03-13 10:45:03 +0500

    form submit

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -1,6 +1,5 @@
 let username = document.querySelector('#login-input-username')
 let password = document.querySelector('#login-input-password')
-let email_input = document.querySelector('#login-input-email')
 let email = document.querySelector('#login-input-email')
 let li = document.querySelectorAll('.login-nav__item')
 let email_label = document.querySelector('#email')
@@ -19,9 +18,9 @@ signIn.onclick = () => {
 
     // make invisible email field
     email_label.style.display = 'none'
-    email_input.style.display = 'none'
+    email.style.display = 'none'
 
-    email_input.required = false
+    email.required = false
 
     btn.innerHTML = 'Sign in'
 }
@@ -36,9 +35,9 @@ signUp.onclick = () => {
 
     // make visible email field
     email_label.style.display = 'block'
-    email_input.style.display = 'inline'
+    email.style.display = 'inline'
 
-    email_input.required = true
+    email.required = true
 
     btn.innerHTML = 'Sign up'
 }
@@ -47,11 +46,101 @@ signUp.onclick = () => {
  * submit handler
  * @param {*} event 
  */
-form.onsubmit = (event) => {
+form.onsubmit = async (event) => {
 
     // freeze page
     event.preventDefault()
 
-    console.log(username.value, email.value, password.value)
+    // req for all users
+    let users = await fetch('http://192.168.1.6:6900/users')
+    users = await users.json()
+
+    // find needed user
+    let filteredUser = users.data.users.find( el => el.userName == username.value)
+
+    // signing up
+    if(btn.textContent == 'Sign up') {
+        
+
+        if(filteredUser) {
+            alert('This user is already exists, please choose another user')
+            username.value = null
+            password.value = null
+            email.value = null
+
+            return
+        }
+
+        if(!validName(username.value)) {
+            alert('Please, this username is too short. Choose another one!')
+            return
+        }
+
+        if(!validPassw(password.value)) {
+            alert("This password is not valid. Password's length should be higher than 3. Choose another one!")
+            return
+        }
+
+        // new user's info
+        let obj = {
+            userId: users.data.users.length ? users.data.users.reduce( (acc, el) => {
+                if(acc < el.userId) {
+                    acc = el.userId
+                }
+                return acc
+            }, 0) + 1 : 1,
+            userName: username.value,
+            email: email.value,
+            password: password.value,
+            date: Date.now()
+        }
+
+        // send post request
+        let data = await fetch('http://192.168.1.6:6900/users', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(obj)
+        })
+
+        // check if request is ok to change location
+       if(data.ok) {
+            window.location = 'http://192.168.1.6:6900/app'
+       }
+       
+       // clean input
+        username.value = null
+        email.value = null
+        password.value = null
+
+        return
+    } else {
+        // check if user is signed up
+        if(!filteredUser) {
+            alert('You have not signed up yet, please sign up first!')
+
+            username.value = null
+            password.value = null
+            return
+        }
+
+        // password check
+        if(filteredUser.password != password.value) {
+            console.log(filteredUser, password.value)
+            alert('Wrong password, please try again')
+            password.value = null
+            return
+        }
+
+        // location change
+        window.location = 'http://192.168.1.6:6900/app'
+
+        // clean input
+        username.value = null
+        password.value = null
+
+        return
+    }
 }
 
