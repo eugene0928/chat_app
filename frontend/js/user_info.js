2518f66a7513bde337f1c91c19a1d2ea09958e15
eugene0928 <umid09tuxtayev@gmail.com>
2022-03-13 23:36:29 +0500

    render users

--- frontend/js/user_info.js
+++ frontend/js/user_info.js
@@ -1,54 +1,90 @@
 let wholeDiv = document.querySelectorAll('.row.sideBar')[0]
-let mainUser = document.querySelector('#mainUser')
+let conversation1 = document.querySelector('#conversation')
+let leftSideDiv = document.querySelector('.row.compose-sideBar')
+let MsgUser = document.querySelector('#mainUser')
+let mainName = document.querySelector('#main-name')
+
+let mainUser = window.localStorage.getItem('user')
 
 window.addEventListener('load', async () => {
-    mainUser.textContent = window.localStorage.getItem('user')
-
+    mainName.textContent = mainUser
     let data = await fetch('http://192.168.1.6:6900/chat')
     data = await data.json()
 
-    let filteredUser = data.data.users.find( el => el.user == mainUser.textContent)
-    console.log(filteredUser)
+    let filteredUser = data.data.users.find( el => el.user == mainUser)
 
+    wholeDiv.innerHTML = null
     for(let user of filteredUser.chat) {
         renderMsgedUser(user)
     }
+
+    let allUsers = await fetch('http://192.168.1.6:6900/users')
+    allUsers = await allUsers.json()
+    let filteredUsers = allUsers.data.users.filter( el => el.userName != mainUser) 
+
+    for(let user of filteredUsers) {
+        renderMsgedUser(user, true)
+    }
 })
 
-function renderMsgedUser(user) {
+function renderMsgedUser(user, left) {
+
     let [div1, div2, div3, img, div4, div5, div6, span, div7, span2] = createEle('div', 'div', 'div', 'img', 'div', 'div', 'div', 'span', 'div', 'span')
     
     div1.classList.add('row', 'sideBar-body')
     div2.classList.add('col-sm-3', 'col-xs-3', 'sideBar-avatar')
     div3.classList.add('avatar-icon')
     
-    img.src = "https://bootdey.com/img/Content/avatar/avatar3.png"
+    img.src = "https://bootdey.com/img/Content/avatar/avatar6.png"
 
     div4.classList.add('col-sm-9', 'col-xs-9', 'sideBar-main')
     div5.classList.add('row')
     div6.classList.add('col-sm-8', 'col-xs-8', 'sideBar-name')
     
     span.classList.add('name-meta')
-    span.textContent = user
+    span.textContent = user.userName
 
     div7.classList.add('col-sm-4', 'col-xs-4', 'pull-right', 'sideBar-time')
     span2.classList.add('time-meta', 'pull-right')
-    span2.textContent = '18:18'
+    span2.textContent = new Date(user.date).toISOString().substr(11, 8).slice(0, 5)
 
     div3.append(img)
     div2.append(div3)
     
     div6.append(span)
-    div5.append(div6)
+    div5.append(div6, div7)
     
     div7.append(span2)
-    div4.append(div5, div7)
+    div4.append(div5)
 
     div1.append(div2, div4)
-    wholeDiv.append(div1)
 
-    div1.addEventListener('click', () => {
-        //
+    if(left) {
+        leftSideDiv.append(div1)
+    } else {
+        wholeDiv.append(div1)
+    }
+
+    div1.addEventListener('click', async () => {
+
+        MsgUser.textContent = user.userName
+        conversation1.innerHTML = null
+
+        let messages = await fetch('http://192.168.1.6:6900/message')
+        messages = await messages.json()
+
+        let userMsg = messages.data.users.find( el => el.user == mainUser)
+
+        let neededMsg = userMsg.data.filter( el => el.user == user.userName)
+
+        if(!userMsg?.data || !neededMsg.length) {
+            alert('no chats yet!')
+            return
+        }
+        
+        for(let i = 0; i < userMsg.data.length; i++) {
+            renderMsg(userMsg.data[i])
+        }
     })
 }
 
